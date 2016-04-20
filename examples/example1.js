f92e481ed3c3ee722c49c2fae858b1a0f514e272
Maik Hummel <yo@beevelop.com>
2016-04-20 23:35:11 +0200

    Use experimental social authentication support

--- examples/example1.js
+++ examples/example1.js
@@ -12,21 +12,47 @@ window.angular.module('ngFeathers.example1', ['ngFeathers'])
       console.log(msg)
     })
 
-    userService.create({
-      username: 'john',
-      password: 'unicorn'
+    /* $feathers.service('/auth/facebook').create({
+      access_token: ''
     }).then(function (res) {
-      $feathers.authenticate({
-        type: 'local',
-        username: 'john',
-        password: 'unicorn'
-      }).then(function (result) {
-        console.log('Authenticated!', result)
-      }).catch(function (error) {
-        console.error('Error authenticating!', error)
-      })
       console.log(res)
     }).catch(function (err) {
       console.error(err)
+    })*/
+
+    $feathers.authenticate({
+      type: 'google',
+      access_token: '...'
+    // type: 'local',
+    // email: 'john@test.de',
+    // password: 'unicorn'
+    }).then(function (result) {
+      console.log('Authenticated!', result)
+      console.log('User', $feathers.get('user'))
+    }).catch(function (error) {
+      console.error(error)
     })
+
+    return
+
+  /* userService.create({
+  username: 'john',
+  email: 'john@test.de',
+  password: 'unicorn'
+  }).then(function (res) {
+  $feathers.authenticate({
+  type: 'facebook-token',
+  access_token: ''
+  // type: 'local',
+  // email: 'john@test.de',
+  // password: 'unicorn'
+  }).then(function (result) {
+  console.log('Authenticated!', result)
+  }).catch(function (error) {
+  console.error('Error authenticating!', error)
+  })
+  console.log(res)
+  }).catch(function (err) {
+  console.error(err)
+  })*/
   })
